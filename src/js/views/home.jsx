f8f33eff2e0a693805e5dda4231663a6d3a41b50
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-12-14 20:33:05 +0000

    stage

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -7,7 +7,6 @@ export const Home = () => {
 
 	const { store, actions } = useContext(Context)
 	return (
-		// "Characters"
 		<div className="bg-dark d-flex flex-column">
 			<h1 className="tittleCharacter text-danger"><strong>Characters</strong></h1>
 			<div className="fatherCharacter d-flex ">
@@ -15,7 +14,6 @@ export const Home = () => {
 					return (<Cards img={`https://starwars-visualguide.com/assets/img/characters/${character.uid}.jpg`} item={character.properties} key={character._id} type={"characters"} id={character.uid} />)})}
 			</div>
 
-			{/* "Planets" */}
 			<div className="Planets">
 				<h1 className="tittleCharacter text-danger"><strong>Planets</strong></h1>
 				<div className="fatherCharacter d-flex text-center mt-5">
@@ -23,7 +21,6 @@ export const Home = () => {
 					return (<Cards img={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`} item={planet.properties} key={planet._id} type={"planets"} id={planet.uid} />)})}
 
 				</div>
-
 			</div>
 		</div>
 	);
