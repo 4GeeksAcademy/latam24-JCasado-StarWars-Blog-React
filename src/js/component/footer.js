b221623e90500aca079571cd11fa4243669ddba4
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2024-01-04 16:13:48 +0000

    ¨Ready¨

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Footer = () => (
-	<div className="card bg-black d-flex justify-content-between container_card">
+	<div className="card bg-black container_card">
 		<footer className="footer bg-transparent text-succes py-3">
 			<p>
 				<strong> Made by {" "}
